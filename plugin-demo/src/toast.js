27a3cd3af57376d34b7a81bbf60568e9b44e0c21
unknown <zhuyeqing1@cn.net.ntes>
2018-03-14 20:09:29 +0800

    v0314

--- plugin-demo/src/toast.js
+++ plugin-demo/src/toast.js
@@ -1,6 +1,19 @@
 var Toast = {};
 Toast.install = function (Vue, options) {
+  let opt = {
+    defaultType: 'bottom',
+    duration: '2500'
+  };
+  for (let property in options) {
+    opt[property] = options[property];
+  }
   Vue.prototype.$toast = (tips, type) => {
+    if (type) {
+      opt.defaultType = type;
+    }
+    if (document.getElementsByClassName('vue-toast').length) {
+      return;
+    }
     let toastTp1 = Vue.extend({
         template: `<div class="vue-toast toast-${type}"> ${tips} </div>`,
       });
@@ -8,7 +21,7 @@ Toast.install = function (Vue, options) {
     document.body.appendChild(tp1);
     setTimeout(function() {
       document.body.removeChild(tp1);
-    }, 2500);
+    }, opt.duration);
   }
 };
 
